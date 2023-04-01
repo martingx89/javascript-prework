afaecb2580496e940570ae485684b0b695c2d8d9
Marcin Adamiak <marcin_89@outlook.com>
2023-04-01 17:01:22 +0200

    Add player interaction

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ if (randomNumber == "1") {
 	computerMove = "nieznany ruch";
 }
 printMessage("Mój ruch: " + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
+console.log("Wpisana odpowiedź to: " + playerInput);
+if (playerInput == "1") {
+	playerMove = "kamień";
+} else if (playerInput == "2") {
+	playerMove = "papier";
+} else if (playerInput == "3") {
+	playerMove = "nożyce";
+} else {
+	console.log("wybrano liczbę poza zakresem, przypisano kamień");
+	playerMove = "kamień";
+}
+printMessage("Twój ruch: " + playerMove);
