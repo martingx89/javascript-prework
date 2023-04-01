37b4f0068b65ba02a2862c429fa9b9e69cf2a71f
Marcin Adamiak <marcin_89@outlook.com>
2023-04-01 18:08:08 +0200

    Fix functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+var argMoveId,
+	argPlayerMove,
+	argComputerMove,
+	computerMove,
+	playerMove,
+	randomNumber,
+	playerInput,
+	argButtonName,
+	buttonRock,
+	buttonPaper,
+	buttonScissors;
+
+buttonRock = document.getElementById("button-rock");
+buttonRock.addEventListener("click", function () {
+	buttonClicked("kamień");
+});
+buttonPaper = document.getElementById("button-paper");
+buttonPaper.addEventListener("click", function () {
+	buttonClicked("papier");
+});
+buttonScissors = document.getElementById("button-scissors");
+buttonScissors.addEventListener("click", function () {
+	buttonClicked("nożyce");
+});
+
+function buttonClicked(argButtonName) {
+	clearMessages();
+	console.log(argButtonName + " został kliknięty");
+	playerMove = argButtonName;
+	console.log("ruch gracza to: " + playerMove);
+	randomNumber = Math.floor(Math.random() * 3 + 1);
+	console.log("wylosowana liczba to: " + randomNumber);
+	computerMove = getMoveName(randomNumber);
+	console.log("ruch komputera to: " + computerMove);
+	displayResult(playerMove, computerMove);
+}
 
 function getMoveName(argMoveId) {
 	console.log("wywołano funkcję getMoveName z argumentem: " + argMoveId);
@@ -18,23 +53,14 @@ function displayResult(argPlayerMove, argComputerMove) {
 	console.log("wywołano funkcję displayResults z argumentami: " + argPlayerMove + ", " + argComputerMove);
 	if (argPlayerMove == "papier" && argComputerMove == "kamień") {
 		printMessage("Wygrywasz!");
-	} else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
-		printMessage("Wygrywasz!");
 	} else if (argPlayerMove == "kamień" && argComputerMove == "nożyce") {
 		printMessage("Wygrywasz!");
+	} else if (argPlayerMove == "nożyce" && argComputerMove == "papier") {
+		printMessage("Wygrywasz!");
 	} else if (argPlayerMove == argComputerMove) {
-		printMessage("Remis!");
+		printMessage("Remis");
 	} else {
 		printMessage("Przegrywasz :(");
 	}
 	printMessage("Zagrałem " + argComputerMove + ", a Ty " + argPlayerMove);
 }
-playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-console.log("wybór ruchu gracza to: " + playerInput);
-playerMove = getMoveName(playerInput);
-console.log("ruch gracza to: " + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log("wylosowana liczba to: " + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log("ruch komputera to: " + computerMove);
-displayResult(playerMove, computerMove);
