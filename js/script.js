f160df0c2e5767e665b7ee75f88063b8e3c5505a
Marcin Adamiak <marcin_89@outlook.com>
2023-04-01 16:36:55 +0200

    Add a text-to-number assignment

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove, playerMove;
-computerMove = "kamień";
-printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
-playerMove = "papier";
-printMessage("Zagrałem " + playerMove);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("wylosowana liczba to: " + randomNumber);
+if (randomNumber == "1") {
+	computerMove = "kamień";
+} else if (randomNumber == "2") {
+	computerMove = "papier";
+} else if (randomNumber == "3") {
+	computerMove = "nożyce";
+} else {
+	computerMove = "nieznany ruch";
+}
+printMessage("Mój ruch: " + computerMove);
