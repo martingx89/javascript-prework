a3c973259cade3cec96c494a41830ee0e617cc09
Marcin Adamiak <marcin_89@outlook.com>
2023-04-01 16:27:18 +0200

    Add script

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = "kamień";
+printMessage("Zagrałem " + computerMove + "! Jeśli Twój ruch to papier, to wygrywasz!");
+playerMove = "papier";
+printMessage("Zagrałem " + playerMove);
